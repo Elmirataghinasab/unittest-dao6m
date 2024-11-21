24c93a6affd760e970728d93ae26861a26f5c6f5
Elmira <taghinasab8395@gmail.com>
2024-11-21 12:24:40 +0330

    Update upload.js

--- ipfs-files/upload.js
+++ ipfs-files/upload.js
@@ -1,12 +1,21 @@
 const https = require("https");
 const FormData = require("form-data");
 const fs = require("fs");
+const readlineSync = require("readline-sync");
 
 const pinataApiKey = "5f72c9cf5a1ad2729bc9";
 const pinataSecretApiKey =
   "334beac3031ce9bcaa83e39f22df0b0049d731dd20bc79638f68a0cc10bb2e85";
 
+// Base path for files
+const basePath = "./ipfs-files/files/";
+
 async function uploadToPinata(filePath) {
+  if (!fs.existsSync(filePath)) {
+    console.error("File does not exist:", filePath);
+    return;
+  }
+
   const data = new FormData();
   data.append("file", fs.createReadStream(filePath));
 
@@ -57,5 +66,11 @@ async function uploadToPinata(filePath) {
   data.pipe(req);
 }
 
-// Example usage
-uploadToPinata("./ipfs-files/files/karlancer.jpg");
+// Get the file name from the user
+const fileName = readlineSync.question(
+  "Enter the file name (with extension): "
+);
+const fullPath = `${basePath}${fileName}`;
+
+// Upload the file to Pinata
+uploadToPinata(fullPath);
